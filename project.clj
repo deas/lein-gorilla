08dd8ae028003dc0b69529aac2ae1e9b6e40ef7c
Andreas Steffan <a.steffan@contentreich.de>
2016-10-28 08:26:48 +0200

    - Quick hack so use the new stuff

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject lein-gorilla "0.3.6"
+(defproject org.clojars.deas/lein-gorilla "0.3.6-SNAPSHOT"
   :description "A Leiningen plugin for the Gorilla REPL."
   :url "https://github.com/JonyEpsilon/lein-gorilla"
   :license {:name "MIT"}
-  :dependencies [[gorilla-repl "0.3.6"]]
+  :dependencies [[org.clojars.deas/gorilla-repl-ng "0.3.6-SNAPSHOT"]]
   :eval-in-leiningen true)
